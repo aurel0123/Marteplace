c8710d377729ee1ca9a8eca47df815a10566945e
kodjogbeaurel5 <kodjogbeaurel5@gmail.com>
2025-03-05 21:30:14 +0100

    Mon premier dep

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Dashboard from '@/features/dashboard/Dashboard'
+import Dashboard from '@/features/Dashboard/Dashboard'
 import Home from '@/features/Dashboard/Home'
 import RegistrationForm from '@/features/auth/Register'
 import Otp from '@/features/auth/Otp'
