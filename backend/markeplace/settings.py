5519ecfb4d563850a77639bcff0f9b4e2a342902
kodjogbeaurel5 <kodjogbeaurel5@gmail.com>
2025-04-17 23:58:21 +0100

    update backend

--- backend/markeplace/settings.py
+++ backend/markeplace/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&q36cy1-t6b^em&%@xe9r^1&8_#3uzu+%l=y8mj#bf07w#-fqy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower()== "true"
-
+#DEBUG = os.environ.get("DEBUG", "False").lower()== "true"
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS" , "127.0.0.1").split(" ")
 
 
